8a54808143a515265af4d34a3051d5cd73efa98a
ajaysagarn <ajaysagarn@gmail.com>
2021-10-19 01:30:35 -0500

    remove unnecessary vars

--- src/main/scala/Job3/JobReducer.scala
+++ src/main/scala/Job3/JobReducer.scala
@@ -11,7 +11,7 @@ import scala.collection.JavaConverters.*
 class JobReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
   val logger = CreateLogger(classOf[JobReducer])
   override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
-    var sum = values.asScala.foldLeft(0)(_ + _.get)
+    val sum = values.asScala.foldLeft(0)(_ + _.get)
     logger.info("Producing reducer output key={} value = {}",key, sum)
     context.write(key, new IntWritable(sum))
   }
--- src/main/scala/Job4/LargestMsgReducer.scala
+++ src/main/scala/Job4/LargestMsgReducer.scala
@@ -12,7 +12,7 @@ class LargestMsgReducer extends Reducer[Text, Text, Text, IntWritable] {
   override def reduce(key: Text, values: lang.Iterable[Text], context: Reducer[Text, Text, Text, IntWritable]#Context): Unit = {
     val vals = values.iterator().asScala
     // Get the length of the largest string from the list of all messages for the log type.
-    var maxCharacters: Int = MapReduceUtils.getMaxMessageCharacters(vals,0)
+    val maxCharacters: Int = MapReduceUtils.getMaxMessageCharacters(vals,0)
     logger.info("Maximum matched message for log level {} is {}",key.toString,maxCharacters)
     context.write(key, new IntWritable(maxCharacters))
   }
